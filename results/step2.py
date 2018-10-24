dc5019614ef26b86de3e9930864617b9799b2333
al153 <al.taylor1105@live.co.uk>
2018-10-24 13:59:13 +0100

    improved graphing

--- results/step2.py
+++ results/step2.py
@@ -6,8 +6,8 @@ import json
 
 from util import *
 
-print(get_most_recent_file_name("data", "step2_"))
-with open(get_most_recent_file_name("data", "step2_"), "r") as f:
+fname = get_most_recent_file_name("data", "step2_")
+with open("data/actual_step2", "r") as f:
     d = json.loads(f.read())['results']
     results = {int(x): d[x] for x in d}
     xs = [int(k) for k in results]
@@ -21,5 +21,9 @@ with open(get_most_recent_file_name("data", "step2_"), "r") as f:
     ax = fig.add_subplot(111)
 
     ax.errorbar(xs, ys, yerr=stds, elinewidth=1, linewidth=2, ecolor='b', color='r')
+    ax.set_xlabel("Number of Threads")
+    ax.set_ylabel("Time to Complete (ms)")
+
+    ax.grid(True)
 
     plt.show()
